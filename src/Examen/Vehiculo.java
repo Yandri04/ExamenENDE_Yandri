e43ce48db5fb48c6eb3ad08ffc74d9324aae84a9
Iñigo <iromanofor@educacion.navarra.es>
2025-12-19 09:37:04 +0100

    vehiculo y alquiler creados

--- src/Examen/Vehiculo.java
+++ src/Examen/Vehiculo.java
@@ -1,5 +1,19 @@
 package Examen;
 
 public class Vehiculo {
+	String modelo;
+	String marca;
+	String matricula;
+	double prestado;
+	public Vehiculo(String modelo, String marca, String matricula, double prestado) {
+		super();
+		this.modelo = modelo;
+		this.marca = marca;
+		this.matricula = matricula;
+		this.prestado = prestado;
+	}
 
+
+
+	
 }
