99c3848336b1e230b5684f26657b7e2553553b5e
Iñigo <iromanofor@educacion.navarra.es>
2025-12-19 09:34:42 +0100

    meotodos creados

--- src/Examen/Vehiculo.java
+++ src/Examen/Vehiculo.java
@@ -1,5 +1,43 @@
 package Examen;
 
 public class Vehiculo {
+	String modelo;
+	String marca;
+	String matricula;
+	double prestado;
 
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public double getPrestado() {
+		return prestado;
+	}
+	public void setPrestado(double prestado) {
+		this.prestado = prestado;
+	}
+	@Override
+	public String toString() {
+		return "Vehiculo [modelo=" + modelo + ", marca=" + marca + ", matricula=" + matricula + ", prestado=" + prestado
+				+ ", getModelo()=" + getModelo() + ", getMarca()=" + getMarca() + ", getMatricula()=" + getMatricula()
+				+ ", getPrestado()=" + getPrestado() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+
+	
 }
